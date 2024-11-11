6318904020522a223b7182b18fe98c3613c4bb2f
Eduardo <eduardobambace@gmail.com>
2024-11-11 14:58:44 +0100

    barra de idiomas

--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,14 @@
-
-
-// Detectar el scroll y mover la navbar
-window.addEventListener('scroll', function() {
-    const navbar = document.getElementById('navbar');
-    
-    // Verificar la posición del scroll
-    if (window.scrollY > 100) { // Ajusta el valor a tu gusto
-      navbar.classList.add('navbar-left'); // Añadir la clase para moverla a la izquierda
-    } else {
-      navbar.classList.remove('navbar-left'); // Si no se hace scroll, regresa a la posición original
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  const bars = document.querySelectorAll(".progress-bar");
+  bars.forEach((bar) => {
+    const level = bar.getAttribute("data-level");
+    bar.style.width = level;
   });
-  
+});
 
 //funcion transicion hero
-window.addEventListener('load', function() {
-    const header = document.querySelector('.hero');
-    header.style.opacity = 1;
-    header.style.transform = 'translateY(1)';
+window.addEventListener("load", function () {
+  const header = document.querySelector(".hero");
+  header.style.opacity = 1;
+  header.style.transform = "translateY(1)";
 });
